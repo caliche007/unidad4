3044a229734823b9b165820af041fbc72f2b58c5
caliche007 <carloschonto2@hotmail.com>
2021-09-12 20:42:23 -0500

    repositorio 1

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+window.onload = init
+
+function = init() 
+
+ new Splide( "#splide" ).mount(), {
+     
+    type: "loop",
+    perPage: 3,
+    rewind: true,
+    height: "50vh",
+    autoplay: true,
+}).mount();
